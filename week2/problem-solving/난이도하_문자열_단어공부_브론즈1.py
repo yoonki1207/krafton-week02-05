bb36c1acafd9aebd35895584765523c2ef6a0866
yoonki1207 <yoonki1207@gmail.com>
2026-03-07 17:28:03 +0900

    [Solved] 난이도하_문자열_단어공부_브론즈1

--- week2/problem-solving/난이도하_문자열_단어공부_브론즈1.py
+++ week2/problem-solving/난이도하_문자열_단어공부_브론즈1.py
@@ -1,2 +1,24 @@
 # 문자열 - 단어 공부 (백준 브론즈1)
 # 문제 링크: https://www.acmicpc.net/problem/1157
+
+s = input().lower()
+cnt = {}
+alphabet = 'abcdefghijklmnopqrstuvwxyz'
+for i in alphabet:
+    cnt[i] = 0
+for i in s:
+    cnt[i] += 1
+m = 0
+k = 'a'
+for key in cnt:
+    if cnt[key] > m: 
+        m = cnt[key]
+        k = key
+w = 0
+for key in cnt:
+    if cnt[key] == m: w += 1
+
+if w > 1:
+    print('?')
+else :
+    print(k.upper())
