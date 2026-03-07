d2533e814a159156eb16ff79d02c94a73600bb5e
yoonki1207 <yoonki1207@gmail.com>
2026-03-07 17:52:09 +0900

    [Solved] 난이도하_정수론_소수찾기_브론즈2

--- week2/problem-solving/난이도하_정수론_소수찾기_브론즈2.py
+++ week2/problem-solving/난이도하_정수론_소수찾기_브론즈2.py
@@ -1,2 +1,16 @@
 # 정수론 - 소수 찾기 (백준 브론즈2)
 # 문제 링크: https://www.acmicpc.net/problem/1978
+import math
+n = int(input())
+arr = map(int, input().split())
+
+def isPrime(x):
+    if x < 2: return False
+    for i in range(2, int(math.sqrt(x)) + 1):
+        if x % i == 0: 
+            return False
+        pass
+    return True    
+
+primes = [x for x in arr if isPrime(x)]
+print(len(primes))
