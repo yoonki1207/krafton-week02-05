1279ae06fa3e9a8874674422deaf928157aefc0d
yoonki1207 <yoonki1207@gmail.com>
2026-03-07 17:54:43 +0900

    [Solved] 난이도하_파이썬문법_최댓값_브론즈3

--- week2/problem-solving/난이도하_파이썬문법_최댓값_브론즈3.py
+++ week2/problem-solving/난이도하_파이썬문법_최댓값_브론즈3.py
@@ -1,2 +1,13 @@
 # 파이썬 문법 - 최댓값 (백준 브론즈3)
 # 문제 링크: https://www.acmicpc.net/problem/2562
+
+arr = [0]*9
+for i in range(9):
+    arr[i] = int(input())
+m = max(arr)
+ans = 0
+for i in range(9):
+    if arr[i] == m:
+        ans = i
+print(m)
+print(ans + 1)
