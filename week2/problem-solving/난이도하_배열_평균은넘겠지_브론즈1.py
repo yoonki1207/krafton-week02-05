9ca82b0da04cfa43241a138fe51682049835a5e5
yoonki1207 <yoonki1207@gmail.com>
2026-03-07 17:38:07 +0900

    [Solved] 난이도하_배열_평균은넘겠지_브론즈1

--- week2/problem-solving/난이도하_배열_평균은넘겠지_브론즈1.py
+++ week2/problem-solving/난이도하_배열_평균은넘겠지_브론즈1.py
@@ -1,2 +1,14 @@
 # 배열 - 평균은 넘겠지 (백준 브론즈1)
 # 문제 링크: https://www.acmicpc.net/problem/4344
+T = int(input())
+for test_case in range(T):
+    s = input().split()
+    n = int(s[0])
+    scores = list(map(int, s[1:]))
+    avg = sum(scores) / n
+    above_avg_n = 0
+    for i in scores:
+        if i > avg:
+            above_avg_n += 1
+    result = above_avg_n/n*100
+    print(f"{result:.3f}%")
