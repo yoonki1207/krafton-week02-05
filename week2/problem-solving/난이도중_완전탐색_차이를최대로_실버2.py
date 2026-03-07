d5f456fa1f928f5964bdb1b0f4bcee0747d453a1
yoonki1207 <yoonki1207@gmail.com>
2026-03-07 22:03:31 +0900

    [Solved] 난이도중_완전탐색_차이를최대로_실버2

--- week2/problem-solving/난이도중_완전탐색_차이를최대로_실버2.py
+++ week2/problem-solving/난이도중_완전탐색_차이를최대로_실버2.py
@@ -1,2 +1,34 @@
 # 완전탐색 - 차이를 최대로 (백준 실버2)
 # 문제 링크: https://www.acmicpc.net/problem/10819
+
+# permutation
+n = int(input())
+line = list(map(int, input().split()))
+
+visited = [False] * n
+
+stk = []
+ans = 0
+def dfs():
+    global stk
+    if len(stk) == n:
+        result = 0
+        for i in range(1, n):
+            result += abs(line[stk[i]] - line[stk[i-1]])
+        global ans
+        ans = max(ans, result)
+        return
+    for i in range(n):
+        if not visited[i]:
+            visited[i] = True
+            stk.append(i)
+            dfs()
+            stk.pop()
+            visited[i] = False
+for i in range(n):
+    visited[i] = True
+    stk.append(i)
+    dfs()
+    stk.pop()
+    visited[i] = False
+print(ans)
