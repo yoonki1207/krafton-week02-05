939ad88388b920a4efafccf16fd11422c08ce40d
yoonki1207 <yoonki1207@gmail.com>
2026-03-07 17:31:15 +0900

    [Solved] 난이도하_문자열_문자열반복_브론즈2

--- week2/problem-solving/난이도하_문자열_문자열반복_브론즈2.py
+++ week2/problem-solving/난이도하_문자열_문자열반복_브론즈2.py
@@ -1,2 +1,10 @@
 # 문자열 - 문자열 반복 (백준 브론즈2)
 # 문제 링크: https://www.acmicpc.net/problem/2675
+T = int(input())
+for test_case in range(T):
+    s = input().split()
+    x, s = int(s[0]), s[1]
+    ans = []
+    for i in s:
+        ans.append(i*x)
+    print(''.join(ans))
