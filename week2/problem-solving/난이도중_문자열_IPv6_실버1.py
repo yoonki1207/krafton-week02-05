3d6b73ca3350821ce39192efd0c1dd3475be3454
yoonki1207 <yoonki1207@gmail.com>
2026-03-07 21:55:04 +0900

    [Solved] 난이도중_문자열_IPv6_실버1

--- week2/problem-solving/난이도중_문자열_IPv6_실버1.py
+++ week2/problem-solving/난이도중_문자열_IPv6_실버1.py
@@ -1,2 +1,11 @@
 # 문자열 - IPv6 (백준 실버1)
 # 문제 링크: https://www.acmicpc.net/problem/3107
+
+ipv6 = input()
+lines = ipv6.split(':')
+if lines[0] == '':
+    lines = lines[1:]
+elif lines[-1] == '':
+    lines = lines[:-1]
+lines = ':'.join([line.zfill(4) if line != '' else ':'.join(['0000']*(8-len(lines)+1)) for line in lines])
+print(lines)
