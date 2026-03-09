4a410b3b9ed1c11baf6f35a221a9f9ec5302664c
yoonki1207 <yoonki1207@gmail.com>
2026-03-09 16:51:28 +0900

    [Solved] 난이도상_정수론_제곱ㄴㄴ수_골드1

--- week2/problem-solving/난이도상_정수론_제곱ㄴㄴ수_골드1.py
+++ week2/problem-solving/난이도상_정수론_제곱ㄴㄴ수_골드1.py
@@ -1,2 +1,20 @@
 # 정수론 - 제곱 ㄴㄴ 수 (백준 골드1)
 # 문제 링크: https://www.acmicpc.net/problem/1016
+
+a, b = map(int, input().split())
+p = 2
+min_q = p*p
+arr = [False] * (b - a + 2)
+ans = b - a + 1
+while p*p <= b:
+    n = a // (p*p)
+    if a % (p*p) != 0: n += 1
+    while n * p * p <= b:
+        if not arr[n * p * p - a]:
+            arr[n * p * p - a] = True
+            ans -= 1
+        n += 1
+    p += 1
+    pass
+
+print(ans)
