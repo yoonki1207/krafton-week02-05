b0f9a5fe0a4ec32877c342b0862251bfcb74db40
yoonki1207 <yoonki1207@gmail.com>
2026-03-13 17:36:35 +0900

    [Solved] 난이도중_분할정복_곱셈_실버1

--- week3/problem-solving/난이도중_분할정복_곱셈_실버1.py
+++ week3/problem-solving/난이도중_분할정복_곱셈_실버1.py
@@ -1,2 +1,13 @@
 # 분할정복 - 곱셈 (백준 실버1)
 # 문제 링크: https://www.acmicpc.net/problem/1629
+
+a, b, c = map(int, input().split())
+
+def solve(a, b, c):
+    if b == 1: return a % c
+    if b % 2 == 0:
+        result = solve(a, b//2, c)
+        return result * result % c
+    else:
+        return a * solve(a, b-1, c) % c
+print(solve(a, b, c))
