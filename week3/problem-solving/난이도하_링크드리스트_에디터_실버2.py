829f4c49dea7f083c449dfe9160e89ea5b5c7ed0
yoonki1207 <yoonki1207@gmail.com>
2026-03-13 10:53:19 +0900

    [Solved] 난이도하_링크드리스트_에디터_실버2

--- week3/problem-solving/난이도하_링크드리스트_에디터_실버2.py
+++ week3/problem-solving/난이도하_링크드리스트_에디터_실버2.py
@@ -1,2 +1,69 @@
 # 링크드리스트 - 에디터 (백준 실버2)
 # 문제 링크: https://www.acmicpc.net/problem/1406
+
+class Node:
+    val = ''
+    prev = None
+    next = None
+
+    def push(self, val):
+        node = Node()
+        node.val = val
+        nextNode = self.next
+        self.next = node
+        node.prev = self
+        if nextNode:
+            nextNode.prev = node
+            node.next = nextNode
+        return node
+    
+    def delete(self):
+        if self.prev == None: return self
+        prev = self.prev
+        next = self.next
+        prev.next = next
+        if next:
+            next.prev = prev
+        return prev
+    
+    def left(self):
+        if self.prev == None: return self
+        return self.prev
+    
+    def right(self):
+        if self.next == None: return self
+        return self.next
+
+s = input()
+T = int(input())
+cursor = Node()
+for c in s:
+    cursor = cursor.push(c)
+for i in range(T):
+    line = input()
+    if len(line) >= 2:
+        line = line.split()
+        # P x
+        cursor = cursor.push(line[1])
+        pass
+    else:
+        if line == 'L':
+            # left
+            cursor = cursor.left()
+            pass
+        elif line == 'D':
+            #right
+            cursor = cursor.right()
+            pass
+        elif line == 'B':
+            # delete a char 
+            cursor = cursor.delete()
+            pass
+while cursor.prev != None:
+    cursor = cursor.prev
+
+printList = []
+while cursor != None:
+    printList.append(cursor.val)
+    cursor = cursor.next
+print(''.join(printList))
