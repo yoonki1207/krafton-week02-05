651439bf600436c0447d363aca4c1374e2520cc8
yoonki1207 <yoonki1207@gmail.com>
2026-03-13 13:46:59 +0900

    [Solved] 난이도하_해시테이블_민균이의비밀번호_브론즈1

--- week3/problem-solving/난이도하_해시테이블_민균이의비밀번호_브론즈1.py
+++ week3/problem-solving/난이도하_해시테이블_민균이의비밀번호_브론즈1.py
@@ -1,2 +1,17 @@
 # 해시 테이블 - 민균이의 비밀번호 (백준 브론즈1)
 # 문제 링크: https://www.acmicpc.net/problem/9933
+
+N = int(input())
+arr = [0 for _ in range(N)]
+s = set()
+for i in range(N):
+    arr[i] = input()
+    s.add(arr[i])
+
+for pwd in arr:
+    rev = ''.join(reversed(pwd))
+    if rev in s:
+        l = len(pwd)
+        m = pwd[l//2]
+        print(l, m)
+        break
