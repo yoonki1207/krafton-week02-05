6e7422bbfabcde0795c145f66161827518747d16
yoonki1207 <yoonki1207@gmail.com>
2026-03-13 16:56:26 +0900

    [Solved] 06_queue

--- week3/basic/06_queue.py
+++ week3/basic/06_queue.py
@@ -44,6 +44,8 @@ def process_print_queue(jobs):
     # TODO: 큐가 비어있지 않은 동안 반복
     ## 큐에서 작업 꺼내기
     ## 작업 처리 (출력 및 리스트에 추가)
+    while len(queue) != 0:
+        processed.append(queue.popleft())
     pass
     
     return processed
