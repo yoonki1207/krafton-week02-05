626bc13c9d5f081b0b1e73311ebefbee4a3cadc9
yoonki1207 <yoonki1207@gmail.com>
2026-03-14 13:58:45 +0900

    [Solved] 난이도상_큐_가운데를말해요_골드2 If N is greater than 100,000, use `sys.stdin.readline`

--- week3/problem-solving/난이도상_큐_가운데를말해요_골드2.py
+++ week3/problem-solving/난이도상_큐_가운데를말해요_골드2.py
@@ -1,2 +1,44 @@
 # 큐 - 가운데를 말해요 (백준 골드2)
 # 문제 링크: https://www.acmicpc.net/problem/1655
+import sys
+input = sys.stdin.readline
+
+MAX_LEN = 20001
+tree = [0] * MAX_LEN * 4
+
+def update(start, end, n, pos, diff):
+    if start > pos or end < pos: return 
+    tree[n] += 1
+    if start == end: return
+    mid = (start + end) // 2
+    update(start, mid, n*2, pos, diff)
+    update(mid + 1, end, n*2+1, pos, diff)
+
+def query(start, end, n, pos): # pos start from 1.
+    if start > end:
+        return None
+    if start == end: return start
+    left_sum = tree[n*2]
+    mid = (start + end) // 2
+    if left_sum >= pos:
+        return query(start, mid, n*2, pos)
+    else:
+        return query(mid+1, end, n*2+1, pos - left_sum)
+    # right_sum = tree[n*2+1] 
+    pass
+
+N = int(input())
+OFFSET = 10000
+for i in range(N):
+    x = int(input())
+    x = x + OFFSET
+    update(0, MAX_LEN, 1, x, 1)
+    pos = i // 2 + 1
+    if i % 2 == 1: # 짝수번째 입력일 때
+        result1 = query(0, MAX_LEN, 1, pos)
+        result2 = query(0, MAX_LEN, 1, pos + 1)
+        result = min(result1, result2)
+    else :
+        result = query(0, MAX_LEN, 1, pos)
+    print(result - OFFSET)
+    pass
