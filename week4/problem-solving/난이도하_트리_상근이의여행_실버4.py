c70201f506137ce9f0eb2ac60dc6875e37ce239a
yoonki1207 <yoonki1207@gmail.com>
2026-03-19 22:47:04 +0900

    [Solved] 난이도하_트리_상근이의여행_실버4

--- week4/problem-solving/난이도하_트리_상근이의여행_실버4.py
+++ week4/problem-solving/난이도하_트리_상근이의여행_실버4.py
@@ -1,2 +1,10 @@
 # 트리 - 상근이의 여행 (백준 실버4)
 # 문제 링크: https://www.acmicpc.net/problem/9372
+
+T = int(input())
+
+for i in range(T):
+    N, M = map(int, input().split())
+    for j in range(M):
+        line = input()
+    print(N - 1)
