f0542cd75e175b7a617e9fe159431c9b9295565a
yoonki1207 <yoonki1207@gmail.com>
2026-03-19 21:58:48 +0900

    [Basic] 02_bst

--- week4/basic/02_bst.py
+++ week4/basic/02_bst.py
@@ -51,12 +51,14 @@ def search_bst(root, target):
         True/False
     """
     # TODO: root가 None이면 False 반환
-    pass
+    if not root: return False
     
     # TODO: 값을 찾으면 True 반환
     ## target이 작으면 왼쪽 서브트리에서 검색
     ## target이 크면 오른쪽 서브트리에서 검색
-    pass
+    if root.value == target: return True
+    elif root.value > target: return search_bst(root.left, target)
+    else: return search_bst(root.right, target)
 
 # 테스트 케이스
 if __name__ == "__main__":
