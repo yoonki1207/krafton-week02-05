24c20b66556e9ce0fb86635b1a7e3cfc6ead8350
yoonki1207 <yoonki1207@gmail.com>
2026-03-19 22:27:20 +0900

    [Solved] 난이도하_그래프DFSBFS_바이러스_실버3

--- week4/problem-solving/난이도하_그래프DFSBFS_바이러스_실버3.py
+++ week4/problem-solving/난이도하_그래프DFSBFS_바이러스_실버3.py
@@ -1,2 +1,26 @@
 # 그래프, DFS, BFS - 바이러스 (백준 실버3)
 # 문제 링크: https://www.acmicpc.net/problem/2606
+
+N = int(input())
+M = int(input())
+arr = [[False for _ in range(N + 1)] for _ in range(N + 1)]
+for i in range(M):
+    a, b = map(int, input().split())
+    arr[a][b] = arr[b][a] = True
+
+visited = [False for _ in range(N + 1)]
+
+from collections import deque
+q = deque([1])
+visited[1] = True
+cnt = 0
+
+while q:
+    front = q.popleft()
+    cnt += 1
+    for index, isCon in enumerate(arr[front]):
+        if isCon and not visited[index]:
+            visited[index] = True
+            q.append(index)
+
+print (cnt - 1)
