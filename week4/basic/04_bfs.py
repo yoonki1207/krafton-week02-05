d1055afcb41c1d4d31d3bfdb27dedb5b2d011176
yoonki1207 <yoonki1207@gmail.com>
2026-03-19 22:06:49 +0900

    [Basic] 04_bfs

--- week4/basic/04_bfs.py
+++ week4/basic/04_bfs.py
@@ -47,13 +47,18 @@ def bfs(graph, start):
     
     # TODO: 큐 생성 및 시작 정점 추가
     ## 방문한 정점 집합
-    pass
+    dq = deque([start])
 
     # TODO: 큐가 빌 때까지 반복
     ## 큐에서 정점 꺼내기
     ## 인접한 정점들 확인
     ## 방문하지 않은 정점이면 큐에 추가
-    pass
+    while dq:
+        front = dq.popleft()
+        if front not in visited:
+            visited.append(front)
+            for next in graph[front]:
+                dq.append(next)
     
     return visited
 
