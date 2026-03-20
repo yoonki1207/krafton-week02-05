40326a726f83149c7e923322da933e82767a13a5
yoonki1207 <yoonki1207@gmail.com>
2026-03-20 10:18:24 +0900

    [Solved] 난이도하_트리_트리만들기_실버4

--- week4/problem-solving/난이도하_트리_트리만들기_실버4.py
+++ week4/problem-solving/난이도하_트리_트리만들기_실버4.py
@@ -1,2 +1,18 @@
 # 트리 - 트리 만들기 (백준 실버4)
 # 문제 링크: https://www.acmicpc.net/problem/14244
+
+N, M = map(int, input().split())
+
+print(0, 1)
+prev_node = 1
+next_node = 2
+while N - 2:
+    N -= 1
+
+    print(prev_node, next_node)
+    if M > 2: # 0과 N-1을 제외한 나머지 모든 리프노드들을 1에서 시작하게 만든다
+        M -= 1
+        next_node += 1
+    else: # 꼬리물기 (리프 노드 생성 안 됨)
+        prev_node = next_node
+        next_node = prev_node + 1
