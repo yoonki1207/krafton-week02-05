5e37dd2f77f164a77683f870921c95f0c3ab8722
yoonki1207 <yoonki1207@gmail.com>
2026-03-21 10:44:36 +0900

    [Solved] 난이도중_트리_트리의부모찾기_실버2

--- week4/problem-solving/난이도중_트리_트리의부모찾기_실버2.py
+++ week4/problem-solving/난이도중_트리_트리의부모찾기_실버2.py
@@ -1,2 +1,27 @@
 # 트리 - 트리의 부모 찾기 (백준 실버2)
 # 문제 링크: https://www.acmicpc.net/problem/11725
+import sys
+sys.setrecursionlimit(10**5 + 1)
+
+N = int(input())
+visited = [False] * (N + 1)
+graph = {}
+for i in range(1, N + 1): graph[i] = []
+for i in range(N - 1):
+    u, v = map(int, input().split())
+    graph[u].append(v)
+    graph[v].append(u)
+# print(graph)
+
+parent = [0] * (N + 1)
+
+def dfs(node):
+    for next_node in graph[node]:
+        if not visited[next_node]:
+            visited[next_node] = True
+            if parent[next_node] == 0:
+                parent[next_node] = node
+            dfs(next_node)
+dfs(1)
+output = '\n'.join(map(str, parent[2:]))
+print(output)
